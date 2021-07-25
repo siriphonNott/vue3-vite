3f2ca8b231fcaf36b8df8a421883ab4e1dff4bf6
nottdev <siriphonnot@gmail.com>
2021-07-25 12:02:51 +0700

    Fix height on contact page

--- src/components/common/RepoBadge.vue
+++ src/components/common/RepoBadge.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex mt-1">
-    <span class="text-gray-200 text-xs bg-gray-700 rounded-l px-4 py-1 flex items-center"><span v-if="icon" class="material-icons text-gray-200 mr-1 text-sm">{{icon}}</span>{{ name }}</span>
+    <span class="text-gray-200 text-xs bg-gray-700 rounded-l px-3 py-1 flex items-center"><span v-if="icon" class="material-icons text-gray-200 mr-1 text-sm">{{icon}}</span>{{ name }}</span>
     <span class="text-gray-200 text-xs bg-gray-500 rounded-r px-2 py-1 flex items-center">{{ count }}</span>
   </div>
 </template>
@@ -10,7 +10,3 @@ export default {
   props: ['name', 'count', 'icon']
 }
 </script>
-
-<style>
-
-</style>
